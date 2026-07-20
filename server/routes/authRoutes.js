b73b7709a24fbb81be1d8a4a6ab2b6a1dd00a7a6
mohammed-owzzz <mohammed.owzzz@gmail.com>
2026-07-21 01:48:32 +0530

    added password reset routes to authRoutes.js

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -5,6 +5,8 @@ import {
   loginUser,
   loginAdmin,
   resendOtp,
+  sendResetOtp,
+  resetPassword,
 } from '../controllers/authController.js'
 import { loginLimiter, otpLimiter, otpVerifyLimiter } from '../middleware/rateLimiter.js'
 
@@ -15,5 +17,7 @@ router.post('/verify-register', otpVerifyLimiter, verifyRegisterOtp)
 router.post('/login', loginLimiter, loginUser)
 router.post('/admin-login', loginLimiter, loginAdmin)
 router.post('/resend-otp', otpLimiter, resendOtp)
+router.post('/forgot-password', otpLimiter, sendResetOtp)
+router.post('/reset-password', otpVerifyLimiter, resetPassword)
 
 export default router
